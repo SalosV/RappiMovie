bec7a854cf64925ff98607b409b880458d0a8874
Santiago Lozada <santiagolozada95@gmail.com>
2021-03-12 00:01:18 -0500

    Configurations

--- app/src/main/java/com/santiagolozada/rappimovie/MainActivity.kt
+++ app/src/main/java/com/santiagolozada/rappimovie/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.santiagolozada.rappimovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Keyboard
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.core.content.ContextCompat
+import dev.chrisbanes.accompanist.coil.CoilImage
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    @Composable
+    fun MyApp(content: @Composable () -> Unit) {
+        MaterialTheme {
+            Surface(color = Color.White) {
+                content()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MyApp {
+                DefaultPreview()
+            }
+        }
+    }
+
+    @Preview(
+        name = "List Movies",
+        showBackground = true,
+        backgroundColor = 0xFFFDFDFD
+    )
+    @Composable
+    fun DefaultPreview() {
+        MyApp {
+
+        }
     }
 }
